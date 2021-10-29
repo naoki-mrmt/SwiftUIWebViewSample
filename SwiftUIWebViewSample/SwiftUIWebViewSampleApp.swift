9592c488f776adab10e48d46bcca9288a1c2522e
Naoki <20cm0135@jec.ac.jp>
2021-10-29 14:19:22 +0900

    Initial Commit

---
+++ SwiftUIWebViewSample/SwiftUIWebViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWebViewSampleApp.swift
+//  SwiftUIWebViewSample
+//
+//  Created by cmStudent on 2021/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWebViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
